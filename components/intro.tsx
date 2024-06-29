33ea49b86568489ff581c1ef8944940d91c6bfdb
PrittoRuban <prittoruban0609@gmail.com>
2024-06-30 01:33:11 +0530

    update intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -77,7 +77,7 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
-        <div className="flex flex-wrap gap-3">
+        <div className="flex flex-wrap justify-center gap-3">
           {" "}
           <Link
             href="#contact"
@@ -108,7 +108,6 @@ export default function Intro() {
           >
             <BsLinkedin />
           </a>
-
           <a
             className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://linktr.ee/prittoruban"
