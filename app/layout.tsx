6d6645337c98791c9f15544c77e4a7aaa51628f2
PrittoRuban <prittoruban0609@gmail.com>
2024-06-29 22:18:17 +0530

    add aditional setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-purple-50 text-gray-950 relative`}
+      >
+        <div
+          className="bg-sky-100 absolute top-[-6rem] right-[11rem] h-[31.25rem] -z-10 w-[31.25rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem]"
+        ></div>
+        <div
+          className="bg-violet-200 absolute top-[-1rem] left-[-35rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem]
+        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        ></div>
+        {children}
+      </body>
     </html>
   );
 }
