ba634e25978971996b1ed97c13a656c1bf862931
PrittoRuban <prittoruban0609@gmail.com>
2024-06-29 21:19:43 +0530

    initail setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pritto Ruban - Portfolio",
+  description:
+    "Pritto Ruban is a full-stack developer, interested in learning new things and Engineer based in India. Loves to build things that make people's lives easier.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
