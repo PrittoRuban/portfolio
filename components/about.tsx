35d8bd3a5337a388421952dc189cffd316f49f05
PrittoRuban <prittoruban0609@gmail.com>
2024-07-08 00:37:51 +0530

    update about page

--- components/about.tsx
+++ components/about.tsx
@@ -1,108 +1,105 @@
 "use client";
-
-import React from "react";
-import SectionHeading from "@/components/UI/section-heading";
 import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
+import { useInView } from "react-intersection-observer";
+import { ChakraProvider, Link } from "@chakra-ui/react";
 
-export default function About() {
-  const { ref } = useSectionInView("About");
+const About = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
 
   return (
-    <main>
-      <motion.section
-        ref={ref}
-        className="relative mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.175 }}
-        id="about"
-      >
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#413e64]"></div>
-        <SectionHeading>About Me</SectionHeading>
+    <ChakraProvider>
+      <div className="relative max-w-4xl mx-auto p-6" id="about" ref={ref}>
+        <div className="bg-[#d9d6ec] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#3b3761]"></div>
+        <motion.h1
+          className="text-2xl font-bold text-center mb-8"
+          initial={{ opacity: 0, y: -20 }}
+          animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+          transition={{ duration: 0.5 }}
+        >
+          Wanna know more about me? You've come to the right place 🎓
+        </motion.h1>
         <motion.p
-          className="mb-3"
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.25 }}
+          className="text-lg mb-4"
+          initial={{ opacity: 0 }}
+          animate={inView ? { opacity: 1 } : { opacity: 0 }}
+          transition={{ delay: 0.5, duration: 0.5 }}
         >
-          Welcome! I'm R. Pritto Ruban, a passionate{" "}
-          <span className="font-bold">
-            Computer Science Engineering student specializing in Cyber Security
-            at R.M.K College of Engineering and Technology
-          </span>
-          , Chennai. I am deeply interested in Full-Stack Development and
-          Blockchain, continuously learning and exploring new technologies to
-          stay ahead in this ever-evolving field.
-        </motion.p>
-        <motion.h4
-          className="mb-2 font-semibold"
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.3 }}
-        >
-          My Journey
-        </motion.h4>
-        <motion.p
-          className="mb-3"
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.35 }}
-        >
-          I completed my schooling at{" "}
-          <span className="font-bold">Vidya Mandir </span>Matric Hr. Sec.
-          School, achieving an impressive{" "}
-          <span className="font-bold">87% in my 12th grade</span>. Currently, I
-          am pursuing my Bachelor of Engineering degree, immersing myself in the
-          world of computer science and engineering.
+          Welcome! I'm{" "}
+          <span className="sm:text-xl font-semibold">R. Pritto Ruban</span>, I
+          completed my schooling at Vidya Mandir Matric Hr. Sec. School,
+          achieving an impressive{" "}
+          <span className="font-bold">87% in my 12th grade.</span>
         </motion.p>
         <motion.p
-          className="mb-3"
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.45 }}
+          className="sm:text-lg mb-4"
+          initial={{ opacity: 0 }}
+          animate={inView ? { opacity: 1 } : { opacity: 0 }}
+          transition={{ delay: 0.5, duration: 0.5 }}
         >
-          I am enthusiastic about attending project presentations and coding
-          events, where I can learn from industry experts and stay updated with
-          the latest technologies.
+          Currently, I am pursuing my{" "}
+          <span className="font-bold">Bachelor of Engineering</span> degree,
+          immersing myself in the world of Computer science and engineering,
+          specializing in Cyber Security at R.M.K College of Engineering and
+          Technology, Chennai.
+        </motion.p>
+        <motion.p
+          className="sm:text-2xl font-bold mb-8"
+          initial={{ opacity: 0 }}
+          animate={inView ? { opacity: 1 } : { opacity: 0 }}
+          transition={{ delay: 1, duration: 0.5 }}
+        >
+          I've solved around 3000 problems across various coding platforms
+          including{" "}
+          <Link
+            href="https://leetcode.com/u/prittoruban/"
+            className="text-blue-600 dark:text-blue-400"
+            target="_blank"
+          >
+            Leetcode ⇗
+          </Link>
+          ,{" "}
+          <Link
+            href="#"
+            className="text-blue-600 dark:text-blue-400"
+            target="_blank"
+          >
+            Geeksforgeeks ⇗
+          </Link>
+          ,{" "}
+          <Link
+            href="#"
+            className="text-blue-600 dark:text-blue-400"
+            target="_blank"
+          >
+            Hackerrank ⇗
+          </Link>
+          , and{" "}
+          <Link
+            href="#"
+            className="text-blue-600 dark:text-blue-400"
+            target="_blank"
+          >
+            Skillrack ⇗
+          </Link>
         </motion.p>
 
         <motion.p
-          className="mb-3"
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.6 }}
+          className="sm:text-2xl font-bold text-center mb-8 mt-8"
+          initial={{ opacity: 0 }}
+          animate={inView ? { opacity: 1 } : { opacity: 0 }}
+          transition={{ delay: 1, duration: 0.5 }}
         >
           My approach to challenges and goals:{" "}
-          <span className=" text-violet-600 dark:text-fuchsia-500">
+          <span className="text-blue-600 dark:text-blue-400">
             Always striving, adapting, and celebrating successes along the way.
           </span>
         </motion.p>
-        <motion.p
-          className="mb-3"
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 0.8 }}
-        >
-          For more about my coding profiles and other projects, visit my{" "}
-          <a
-            href="https://linktr.ee/prittoruban"
-            className="text-blue-500 underline"
-            target="_blank"
-          >
-            Linktree
-          </a>
-          .
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.85 }}
-        >
-          Thank you for visiting my portfolio. Let's achieve the impossible
-          together!
-        </motion.p>
-      </motion.section>
-    </main>
+      </div>
+    </ChakraProvider>
   );
-}
+};
+
+export default About;
