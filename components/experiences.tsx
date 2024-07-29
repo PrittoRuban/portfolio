3b8000558a1d90cd4e37843bfb621761c9f70b65
PrittoRuban <prittoruban0609@gmail.com>
2024-07-29 22:47:24 +0530

    Update experiences section with background blur effect

--- components/experiences.tsx
+++ components/experiences.tsx
@@ -9,7 +9,12 @@ export default function Experiences() {
   const { ref } = useSectionInView("Experience", 0.5);
 
   return (
-    <section ref={ref} id="experiences" className="scroll-mt-28 mb-28 max-w-screen-md">
+    <section
+      ref={ref}
+      id="experiences"
+      className="relative scroll-mt-28 mb-28 max-w-screen-md"
+    >
+      <div className="bg-[#fceded] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#3b2727]"></div>
       <SectionHeading>My Experiences</SectionHeading>
       <div>
         {experiencesData.map((experience, index) => (
